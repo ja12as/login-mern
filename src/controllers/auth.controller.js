b34ec68b78851ca5ee7b1eb4f42b2b53f5973748
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2024-02-22 22:18:11 -0500

    LOGIN-MERN_2_EDICCION

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -29,6 +29,44 @@ export const register = async(req , res) => {
 
 };
 
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: 'El usuario no existe' });
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({ message: 'La credencial es incorrecta' });
+
+        const token = await createTokenAccess({ id: userFound._id });
+        res.cookie('token', token)
+        return res.status(201).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email
+        });
+    } catch (error) {
+        // Capturamos y devolvemos el error como respuesta
+        return res.status(500).json({ message: error.message });
+    }
+};
 
 
-export const login = (req , res) => res.send("login");
+
+export const logout = (req, res)=>{
+    res.cookie('token','',
+    {expires:new Date()}).json({message:"Cerró sesión correctamente"});
+    return ren.sendStatus(200);
+};
+
+export const profile = async(req ,res)=>{
+    const userFound =await User.findById(req.user.id);
+    if(!userFound)return  res.status(400).json({message:'No se encontró al usuario'});
+
+    res.status(201).json({
+        id: userFound._id,
+        username: userFound.username,
+        email :  userFound.email
+    })
+
+};
