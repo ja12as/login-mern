896e289cb54501a925a10be19a23a3b57e7f21a9
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2024-02-26 09:24:34 -0500

    LOGIN-MERN_3.1_EDICCION

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { register, login , logout, profile} from "../controllers/auth.controller.js";
 import { requiredAuth } from "../middlewares/tokenValidation.js";
 import { registerSchema , loginSchema} from "../schemas/auth.schema.js";
-import { validateschema } from "../middlewares/validator.middewares.js";
+import { validateSchema } from "../middlewares/validator.middewares.js";
 
 const router = Router();
 
-router.post('/register',validateschema(registerSchema),register);
-router.post('/login',validateschema(loginSchema), login);
+router.post('/register',validateSchema(registerSchema),register);
+router.post('/login',validateSchema(loginSchema), login);
 router.post('/logout', logout);
 router.get('/profile',requiredAuth, profile);
 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,14 @@
 import { Route, Router } from "express";
 import { getTask, getTasks, createTask ,deleteTask,updateTask } from "../controllers/task.controller.js";
 import {requiredAuth } from '../middlewares/tokenValidation.js'
+import { createTaskSchema } from '../schemas/task.schema.js';
+import { validateSchema } from "../middlewares/validator.middewares.js";
 
 const router = Router();
 
 router.get('/tasks', requiredAuth, getTasks)
 router.get('/task/:id', requiredAuth, getTask)
-router.post('/task', requiredAuth, createTask)
+router.post('/task', requiredAuth, validateSchema(createTaskSchema), createTask)
 router.put('/task/:id', requiredAuth, updateTask)
 router.delete('/task/:id', requiredAuth, deleteTask)
 
